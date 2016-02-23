ad4614927f135dad4a93ae2d4e425271f7383c06
ledrou_83 <simondrouin4@gmail.com>
2016-02-22 22:42:58 -0500

    second draft lister perdu

--- biblio.sh
+++ biblio.sh
@@ -107,9 +107,9 @@ function init {
 
 function lister {
     if [[ $2 =~ "--perdu" ]] ; then
-        awk -F'%' '{ if( $1 != "" ) { printf $1 " :: " ; printf "%-10s", "[ "$4"   ] " ; printf "\""$3"\" " ; print $5} }' $depot
+        awk -F'%' '{ if( $1 != "" && $5 != "<<PERDU>>" ) { printf $1 " :: " ; printf "%-10s", "[ "$4"   ] " ; printf "\""$3"\" "; print $5 } }' $depot
     else
-        awk -F'%' '{ if( $1 != "" && $5 != "<<PERDU>>" ) { printf $1 " :: " ; printf "%-10s", "[ "$4"   ] " ; print "\""$3"\"" } }' $depot
+        awk -F'%' '{ if( $1 != "") { printf $1 " :: " ; printf "%-10s", "[ "$4"   ] " ; printf "\""$3"\""} }' $depot
     fi
 
     return $(( $# - 1 ))
